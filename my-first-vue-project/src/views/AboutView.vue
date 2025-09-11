c63a51da8556a4a9e61ffc41b266c28208354203
joaoeferrari <ferrarijoao40@gmail.com>
2025-09-11 11:30:36 -0300

    learning composables

--- my-first-vue-project/src/views/AboutView.vue
+++ my-first-vue-project/src/views/AboutView.vue
@@ -1,6 +1,15 @@
+<script setup>
+  
+  import { useFlash } from '@/composables/useFlash';
+
+  let { flash } =  useFlash();
+
+</script>
+
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <button @click="flash('Yay!','It  Works On The About Page!', 'info')" >Click Me</button>
   </div>
 </template>
 
--- my-first-vue-project/src/views/HomeView.vue
+++ my-first-vue-project/src/views/HomeView.vue
@@ -1,9 +1,38 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+  
+  import { useFlash } from '@/composables/useFlash';
+  import { ref, watch } from 'vue';
+  import { useStorage } from '@/composables/useStorage';
+
+  let { flash } =  useFlash();
+
+  let food = useStorage('food', 'tacos');
+  let age = useStorage('age', 10);
+
+  let obj = useStorage('obj', {one: 'one'});
+
+  setTimeout(()=>{
+    obj.value.one = 'CHANGED;'
+  }, 3000)
+
 </script>
 
+
+<!-- template é a parte visual -->
 <template>
   <main>
-    <TheWelcome />
+    <p>
+      What is your favorite food? <input type="text" v-model="food" >
+    </p>
+
+    <p>
+      How old are you? <input type="text" v-model="age" >
+    </p>
+
+    <p>
+      <button @click="flash('Tests', 'It Works')"> Click me </button>
+    </p>
+
+
   </main>
 </template>
